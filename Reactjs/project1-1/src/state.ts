b07c5fcec6811e073d6a998e9f76988cacf8a7b4
vladimirandropov <vvvvvvvvvv@inbox.ru>
2026-09-04 19:27:27 +0300

    js

--- Reactjs/project1-1/src/state.ts
+++
@@ -1,5 +0,0 @@
-const {BehaviorSubject} = window.rxjs;
-
-/** Поток, содержащий значения громкости */
-export const value$ = new BehaviorSubject(50);
-export const lastNonZero$ = new BehaviorSubject(50);
